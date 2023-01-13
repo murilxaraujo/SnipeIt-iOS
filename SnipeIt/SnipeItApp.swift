b9f54fc47edf24cf57347f9c5e856729c029906c
Murilo Araujo <murilo@muriloaraujo.com>
2023-01-13 20:15:28 -0300

    Initial Commit

---
+++ SnipeIt/SnipeItApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnipeItApp.swift
+//  SnipeIt
+//
+//  Created by Murilo Araujo on 13/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SnipeItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
